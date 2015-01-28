98e4e9317897541155b4eda14198d6d9f1d24c55
eric bartsch <bartsched@gmail.com>
2015-01-28 15:01:21 -0700

    added a terms page

--- gcamp_app/config/routes.rb
+++ gcamp_app/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 root 'welcome#index'
+get '/terms', to: 'terms#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
