966c4b50fbaef3cde41584eef9c8058fbfb62ffa
eric bartsch <bartsched@gmail.com>
2015-01-28 15:52:18 -0700

    added about page

--- gcamp_app/config/routes.rb
+++ gcamp_app/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 root 'welcome#index'
 get '/terms', to: 'terms#index'
+get '/about', to: 'about#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
