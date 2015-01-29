0466ad1dd81b4f3f499223514288c9712282ee4d
eric bartsch <bartsched@gmail.com>
2015-01-29 16:09:48 -0700

    added quotes on homepage

--- gcamp_app/app/controllers/welcome_controller.rb
+++ gcamp_app/app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    @quotes_array = [
+      {author: "- Cayla Hayes", content: "gCamp has changed my life!  It's the best tool I've ever used."},
+      {author: "- Leta Jaskolski", content: "Before gCamp I was a disorderly slob.  Now I'm more organized than I've ever been"},
+      {author: "- Lavern Upton", content: "Don't hesitate - sign up right now!  You'll never be the same."}
+    ]
   end
 end
