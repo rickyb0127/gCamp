9ebd621ca43a19158023f2596c10308033292748
eric bartsch <bartsched@gmail.com>
2015-02-19 15:30:47 -0700

    added projects crud

---
+++ gcamp_app/app/controllers/projects_controller.rb
@@ -0,0 +1,51 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      flash[:notice] = "Project was successfully created"
+      redirect_to project_path(@project)
+    else
+      @project = Project.new(project_params)
+      render :new
+    end
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      flash[:notice] = "Project was successfully updated"
+      redirect_to action: "show"
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    flash[:notice] = "Project was successfully deleted"
+    redirect_to projects_path
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name)
+  end
+end
--- gcamp_app/config/routes.rb
+++ gcamp_app/config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get '/users', to: 'users#index'
   resources :tasks
   resources :users
+  resources :projects
 end
