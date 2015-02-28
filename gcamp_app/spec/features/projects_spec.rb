44e5c1cdcb42ef8e921beb2481bdcd5468e11770
eric bartsch <bartsched@gmail.com>
2015-02-28 12:19:52 -0700

    added model and capybara testing to projects

--- gcamp_app/spec/features/projects_spec.rb
+++ gcamp_app/spec/features/projects_spec.rb
@@ -1,1 +1,18 @@
 require 'rails_helper'
+
+feature 'Projects' do
+
+  scenario 'User can see a flash message' do
+    Project.create!(
+      name: "new project"
+    )
+
+    visit projects_path
+    click_on 'New Project'
+    fill_in 'Name', with: 'test'
+    click_on 'Create Project'
+
+    expect(page).to have_content('Project was successfully created')
+  end
+
+end
