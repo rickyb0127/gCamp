1b756badb425b44c303e2dc8e25a2775c1062d31
eric bartsch <bartsched@gmail.com>
2015-02-28 12:00:37 -0700

    added model and capybara testing to users

--- gcamp_app/spec/features/users_spec.rb
+++ gcamp_app/spec/features/users_spec.rb
@@ -1,1 +1,20 @@
 require 'rails_helper'
+
+feature 'Users' do
+
+  scenario 'User can see a flash message' do
+    User.create!(
+      first_name: 'Joe',
+      last_name: 'Randa',
+      email: 'joeranda@example.com'
+    )
+
+    visit users_path
+    click_on 'Edit'
+    fill_in 'First Name', with: 'Joke'
+    click_on 'Update User'
+
+    expect(page).to have_content('User was successfully updated')
+  end
+
+end
