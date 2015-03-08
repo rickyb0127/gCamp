384a33d7a72dc185ca6d129a83046e9e0e0ac07d
eric bartsch <bartsched@gmail.com>
2015-03-07 21:42:39 -0700

    users can add members to projects and change their roles

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,4 +1,43 @@
 class MembershipsController < ApplicationController
+  before_action :set_project
+
   def index
+    @membership = @project.memberships.new
+  end
+
+  def create
+    @membership = @project.memberships.new(membership_params)
+    if @membership.save
+      flash[:notice] = @membership.user.full_name + " " + "was successfully added"
+      redirect_to project_memberships_path(@project)
+    else
+      render :index
+    end
+  end
+
+  def update
+    @membership = Membership.find(params[:id])
+    if @membership.update(membership_params)
+      flash[:notice] = @membership.user.full_name + " " + "was successfully updated"
+      redirect_to project_memberships_path(@project)
+    else
+      render :index
+    end
+  end
+
+  def destroy
+    @membership = Membership.find(params[:id])
+    @membership.destroy
+    redirect_to project_memberships_path(@project)
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def membership_params
+    params.require(:membership).permit(:user_id, :project_id, :role)
   end
 end
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,7 @@
 class Membership < ActiveRecord::Base
+  ROLE = ["Member", "Owner"]
+  validates :role, inclusion: ROLE
+
   belongs_to :user
   belongs_to :project
 end
