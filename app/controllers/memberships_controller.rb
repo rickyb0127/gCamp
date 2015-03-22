e048ca11f335dcc6815ea397968b629ecd9e9323
eric bartsch <bartsched@gmail.com>
2015-03-22 10:47:51 -0600

    nobody should be able to remove the last owner of a project

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -2,6 +2,7 @@ class MembershipsController < ApplicationController
   before_action :set_project
   before_action :authorize_project, only: [:index]
   before_action :authorize_owner, only: [:create, :update]
+  before_action :ensure_at_least_one_owner, only: [:update]
 
   def index
     @membership = @project.memberships.new
@@ -48,6 +49,14 @@ class MembershipsController < ApplicationController
     unless @project.memberships.where(user_id: current_user.id).pluck(:role) == ["Owner"]
       flash[:error] = "You do not have access"
       redirect_to project_path(@project)
+    end
+  end
+
+  def ensure_at_least_one_owner
+    @membership = Membership.find(params[:id])
+    if @membership.role == "Owner" && @project.memberships.where(role: "Owner").count <= 1
+      flash[:error] = "Projects must have at least one owner"
+      redirect_to project_memberships_path(@project)
     end
   end
 end
