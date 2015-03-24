53a48af5e11da432119b809590e1126b0acc3547
eric bartsch <bartsched@gmail.com>
2015-03-23 18:01:59 -0600

    fix permissions so members cannot hack params to edit project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   before_action :authorize
   before_action :set_project, only: [:edit, :update, :show, :destroy]
   before_action :authorize_project, only:[:show]
-  before_action :owner_or_admin, only:[:edit, :destroy]
+  before_action :owner_or_admin, only:[:edit, :destroy, :update]
 
   def index
     @projects = current_user.projects
