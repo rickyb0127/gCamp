eec22bf802c828d261cc7a7b7fe6aa05fd482963
eric bartsch <bartsched@gmail.com>
2015-03-29 14:51:29 -0600

    add controller spec for registations

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -1,12 +1,26 @@
 require 'rails_helper'
 
-# describe RegistrationsController do
-#   describe "#create" do
-#     it 'allows users do' do
-#       user = create_user
-#       session[:user_id] = user.id
-#       get :index
-#       expect(response). to be_success
-#     end
-#   end
-# end
+describe RegistrationsController do
+  describe "GET #new" do
+    it 'creates a new instance of a user' do
+
+      get :new
+
+      expect(assigns(:user)).to be_a_new(User)
+    end
+  end
+
+  describe "POST #create" do
+    describe "on success" do
+      it 'create a new user with valid parameters' do
+
+        expect {
+          post :create, user: { first_name: 'new', last_name: 'guy', email: 'blue@example.com', password: 'wookie' }
+        }.to change { User.count }.by(1)
+
+        expect(flash[:notice]).to eq "You have successfully signed up"
+        expect(response).to redirect_to new_project_path
+      end
+    end
+  end
+end
