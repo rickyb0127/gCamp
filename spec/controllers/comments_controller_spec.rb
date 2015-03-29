a10dd548bac2c868c5dbc887aa1fa0302cf4f90b
eric bartsch <bartsched@gmail.com>
2015-03-29 16:22:35 -0600

    add controller specs for comments

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,22 +1,21 @@
 require 'rails_helper'
 
-# describe CommentsController do
-#   describe "POST #create" do
-#     describe "on success" do
-#       it "creates a new comment with valid parameters" do
-#         expect {
-#           user = create_user
-#           project = create_project
-#           membership = create_membership
-#           session[:user_id] = user.id
-#           task = create_task(project_id: project.id)
-#
-#           post :create, project_id: project.id, task_id: task.id, id: comment.id
-#         }.to change { Comment.count }.by(1)
-#
-#         expect(Comment.last.body).to eq "new comment"
-#         expect(response.status).to eq(302)
-#       end
-#     end
-#   end
-# end
+describe CommentsController do
+  describe "POST #create" do
+    describe "on success" do
+      it "creates a new comment with valid parameters" do
+        user = create_user
+        project = create_project
+        session[:user_id] = user.id
+        task = create_task(project_id: project.id)
+
+        expect {
+          post :create, project_id: project.id, task_id: task.id, comment: { body: "i am a new comment" }
+        }.to change { Comment.count }.by(1)
+
+        expect(Comment.last.body).to eq "i am a new comment"
+        expect(response.status).to eq(302)
+      end
+    end
+  end
+end
